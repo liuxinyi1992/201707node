da58523fa30be9256350bd03d77a60beff4a5a40
zhufengnodejs <zhufengnodejs@126.com>
2017-09-16 10:37:16 +0800

    express定义路由

--- 1.express/2.express.js
+++ 1.express/2.express.js
@@ -11,8 +11,26 @@ let app = express();
 app.get('/',function(req,res){
   res.end('home');
 });
+
 //当客户端以GET方式访问服务器/user路径的时候，会由哪个函数来进行处理
 app.get('/user',function(req,res){
   res.end('user');
 });
+/*app.post();
+app.put();
+app.delete();*/
+// all 能匹配所有的方法，不管客户端请求过来的方法名是什么，都能匹配上
+// * 代表所有的路径
+app.all('*',function(req,res){
+  res.end('Not Found');
+});
+
 app.listen(8080);
+/**
+app.listen = function listen(port) {
+  var server = require('http').createServer(app);
+  return server.listen.apply(server, port);
+
+  require('http').createServer(app).listen(port);
+};
+ **/
