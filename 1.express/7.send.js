74829099fa89ad1aac92fe709a1eb207819f757d
zhufengnodejs <zhufengnodejs@126.com>
2017-09-16 15:54:47 +0800

    中间件

--- 1.express/7.send.js
+++ 1.express/7.send.js
@@ -7,10 +7,40 @@
  */
 let express = require('express');
 let app = express();
+let statusCodes = {
+  200:'OK',
+  206:'Partial Content',//部分内容 下载
+  301:'Moved Permanently',//永久重定向
+  302:'Found',//临时重定向
+  304:'Not Modified',//缓存发现
+  400:'Bad Request', //客户端请求错误
+  401:'Unauthorized',//未授权的
+  403:'Forbidden',//无权访问
+  404:'Not Found',//资源未找到
+  500:'Internal Server Error', //服务器内部错误
+  503:'Service Unavailable' //服务器暂时不可用
+}
+//添加公共的方法，可以接收任何类型的响应体
+app.use(function(req,res,next){
+  res.send = function(params){
+    switch(typeof params){
+      case 'object':
+        params = JSON.stringify(params);//把对象转成字符串
+        break;
+      case 'number':
+        res.statusCode = params;
+        params = statusCodes[params];
+      default:
+        break;
+    }
+    res.end(params);
+  }
+  next();
+});
 let users = [];
 // curl http://localhost:8080/users  获取用户数组
 app.get('/users',function(req,res){
- res.end(JSON.stringify(users));
+ res.send(users);
 });
 // post /users name=zfpx   增加一个用户
 // curl -X POST --data '{"name":"zfpx"}' http://localhost:8080/users
@@ -25,8 +55,7 @@ app.post('/users',function(req,res){
     let user = JSON.parse(result);//把result从字符串转成对象
     user.id = Date.now();
     users.push(user);
-    console.log(users);
-    res.end(JSON.stringify(user));
+    res.send(user);
   });
 });
 
---
+++ test.js
@@ -0,0 +1,5 @@
+/**
+ * 珠峰培训 http://www.zhufengpeixun.cn
+ */
+require('_http_server')
+console.log(require('_http_server').STATUS_CODES);
